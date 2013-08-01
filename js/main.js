c7ed9b742da413010f03571c769fa4c35430b431
Maxime ALAY-EDDINE <maxime.alayeddine@gmail.com>
2013-08-01 07:37:37 +0200

    First commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
+var bgfixed = $('#bg-fixed');
+var menu = $('#menu');
+var menuButtons = menu.find('li a');
+var content = $('#content');
 
+function switchMode() {
+        if(menu.hasClass('top')) {
+            menu.removeClass('top');
+            content.removeClass('top');
+        } else {
+            menu.addClass('top');
+            content.addClass('top');
+        }
+}
+
+$(document).load(function() {
+});
+$(document).ready(function() {
+    menu.on('click', '.invert', function () {
+        switchMode();
+    });
+    bgfixed.on('mouseover', '#bg-logo', function () {
+        switchMode();
+        menuButtons.first().addClass('active');
+    });
+    menu.on('click', 'a', function () {
+        menuButtons.removeClass('active');
+        $(this).addClass('active');
+    });
+});
